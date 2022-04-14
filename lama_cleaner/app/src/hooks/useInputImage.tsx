2b031603ed73b81aaf15a0a942d21ca3142997c4
Sanster <cwq1913@gmail.com>
2022-04-14 22:22:18 +0800

    save useInputImage with correct filename

--- lama_cleaner/app/src/hooks/useInputImage.tsx
+++ lama_cleaner/app/src/hooks/useInputImage.tsx
@@ -8,14 +8,21 @@ export default function useInputImage() {
     headers.append('pragma', 'no-cache')
     headers.append('cache-control', 'no-cache')
 
-    fetch('/inputimage', { headers })
-      .then(res => res.blob())
-      .then(data => {
-        if (data && data.type.startsWith('image')) {
-          const userInput = new File([data], 'inputImage')
-          setInputImage(userInput)
-        }
-      })
+    fetch('/inputimage', { headers }).then(async res => {
+      const filename = res.headers
+        .get('content-disposition')
+        ?.split('filename=')[1]
+        .split(';')[0]
+
+      const data = await res.blob()
+      if (data && data.type.startsWith('image')) {
+        const userInput = new File(
+          [data],
+          filename !== undefined ? filename : 'inputImage'
+        )
+        setInputImage(userInput)
+      }
+    })
   }, [setInputImage])
 
   useEffect(() => {
