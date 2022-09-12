f037331c9a2ba2ce29fd5a0b98734a663ec7379f
Ahmed Hesham <hesham@huspy.io>
2022-09-12 12:45:21 +0200

    adding docker compose

--- lama_cleaner/parse_args.py
+++ lama_cleaner/parse_args.py
@@ -5,7 +5,7 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--host", default="127.0.0.1")
+    parser.add_argument("--host", default="0.0.0.0")
     parser.add_argument("--port", default=8080, type=int)
     parser.add_argument("--model", default="lama", choices=["lama", "ldm", "zits", "mat", 'fcf'])
     parser.add_argument("--device", default="cuda", type=str, choices=["cuda", "cpu"])
