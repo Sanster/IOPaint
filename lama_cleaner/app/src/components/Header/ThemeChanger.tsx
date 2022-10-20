c6268d52b26c959d6341d128439f8764a3f9698d
Qing <cwq1913@gmail.com>
2022-10-20 21:06:53 +0800

    use dark theme by default

--- lama_cleaner/app/src/components/Header/ThemeChanger.tsx
+++ lama_cleaner/app/src/components/Header/ThemeChanger.tsx
@@ -4,20 +4,11 @@ import { SunIcon, MoonIcon } from '@heroicons/react/outline'
 
 export const themeState = atom({
   key: 'themeState',
-  default: 'light',
+  default: 'dark',
 })
 
 export const ThemeChanger = () => {
   const [theme, setTheme] = useRecoilState(themeState)
-
-  useEffect(() => {
-    const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)')
-    if (darkThemeMq.matches) {
-      setTheme('dark')
-    } else {
-      setTheme('light')
-    }
-  }, [setTheme])
 
   const themeSwitchHandler = () => {
     const newTheme = theme === 'light' ? 'dark' : 'light'
