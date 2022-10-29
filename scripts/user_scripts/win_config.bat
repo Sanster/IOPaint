0414fba9ca0d26c828993b07e1b4b6e84369b23d
Mingyi Jin <jinmingyi1998@sina.cn>
2022-10-29 18:04:58 +0800

    feat: add choice cuda

--- scripts/user_scripts/win_config.bat
+++ scripts/user_scripts/win_config.bat
@@ -6,7 +6,14 @@ set PATH=C:\Windows\System32;%PATH%
 
 @call conda-unpack
 
-@call conda install -c pytorch -c conda-forge -y pytorch=1.12.1=py3.10_cuda11.6_cudnn8_0
+:CHOICE_CUDA
+echo DO YOU WANT TO INSTALL WITH CUDA? (y/n)
+set /p Input=Enter y or n:
+if "%Input%"=="y" (
+   @call conda install -c pytorch -c conda-forge -y pytorch=1.12.1=py3.10_cuda11.6_cudnn8_0
+) else if "%Input%"=="n" (
+   @call conda install -c pytorch -c conda-forge -y pytorch=1.12.1=py3.10_cpu_0
+) else (goto CHOICE_CUDA)
 
 @call pip3 install -U lama-cleaner
 
