b11780f494d83d59e9facaf148f6b79a6bfd565a
Qing <cwq1913@gmail.com>
2022-11-06 08:56:22 +0800

    fix win_config.bat

--- scripts/user_scripts/win_config.bat
+++ scripts/user_scripts/win_config.bat
@@ -7,7 +7,8 @@ set PATH=C:\Windows\System32;%PATH%
 @call conda-unpack
 
 @call conda install -y cudatoolkit=11.3
-@call pip3 install torch --extra-index-url https://download.pytorch.org/whl/cu113
+@call pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113
 @call pip3 install -U lama-cleaner
+
 
 @call invoke config
