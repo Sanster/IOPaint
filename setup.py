ffa6b7c343d3d112e4d341fb74a8a12bb84ad10e
Qing <cwq1913@gmail.com>
2022-11-18 22:26:23 +0800

    0.27.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def load_requirements():
 # https://setuptools.readthedocs.io/en/latest/setuptools.html#including-data-files
 setuptools.setup(
     name="lama-cleaner",
-    version="0.26.2",
+    version="0.27.0",
     author="PanicByte",
     author_email="cwq1913@gmail.com",
     description="Image inpainting tool powered by SOTA AI Model",
