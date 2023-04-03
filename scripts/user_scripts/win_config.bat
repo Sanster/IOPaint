bf19deec735e13e654f52e9d24a4c445358a0d3c
Qing <cwq1913@gmail.com>
2023-04-03 21:51:29 +0800

    update

--- scripts/user_scripts/win_config.bat
+++ scripts/user_scripts/win_config.bat
@@ -8,7 +8,7 @@ set PATH=C:\Windows\System32;%PATH%
 
 @call conda install -y -c conda-forge cudatoolkit=11.7
 @call pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 --extra-index-url https://download.pytorch.org/whl/cu117
-@call pip install xformers
+@call pip install xformers==0.0.16
 @call pip3 install -U lama-cleaner
 @call lama-cleaner --install-plugins-package
 
