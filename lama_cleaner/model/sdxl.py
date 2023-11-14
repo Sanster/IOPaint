2c9a53da8e18d7ec3e4b5ae87089f8c8da5a4ccf
Qing <cwq1913@gmail.com>
2023-11-14 15:24:30 +0800

    sdxl do not has nsfw

--- lama_cleaner/model/sdxl.py
+++ lama_cleaner/model/sdxl.py
@@ -22,15 +22,6 @@ class SDXL(DiffusionInpaintModel):
         model_kwargs = {
             "local_files_only": kwargs.get("local_files_only", kwargs["sd_run_local"])
         }
-        if kwargs["disable_nsfw"] or kwargs.get("cpu_offload", False):
-            logger.info("Disable Stable Diffusion Model NSFW checker")
-            model_kwargs.update(
-                dict(
-                    safety_checker=None,
-                    feature_extractor=None,
-                    requires_safety_checker=False,
-                )
-            )
 
         use_gpu = device == torch.device("cuda") and torch.cuda.is_available()
         torch_dtype = torch.float16 if use_gpu and fp16 else torch.float32
