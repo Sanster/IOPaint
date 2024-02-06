cf9ceea4e628d970ebf2e534e3c167d3769dc3c1
Qing <cwq1913@gmail.com>
2024-02-06 22:01:09 +0800

    update

--- scripts/user_scripts/win_config.bat
+++ scripts/user_scripts/win_config.bat
@@ -9,7 +9,7 @@ set PATH=C:\Windows\System32;%PATH%
 @call conda install -y -c conda-forge cudatoolkit=11.8
 @call pip install torch==2.1.2 torchvision==0.16.2 --extra-index-url https://download.pytorch.org/whl/cu118
 @call pip3 install -U iopaint
-@call iopaint install-plugins-package
+@call iopaint install-plugins-packages
 
 @call iopaint start-web-config --config-file %0\..\installer_config.json
 
