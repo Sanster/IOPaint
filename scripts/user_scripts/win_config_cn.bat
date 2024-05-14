dc4b9690c3e619303d9df1d121a55324619c6921
Qing <cwq1913@gmail.com>
2024-05-14 11:04:56 +0800

    Update win_config_cn.bat

--- scripts/user_scripts/win_config_cn.bat
+++ scripts/user_scripts/win_config_cn.bat
@@ -6,15 +6,7 @@ set PATH=C:\Windows\System32;%PATH%
 
 @call conda-unpack
 
-@call conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
-@call conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
-@call conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/pytorch/
-@call conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/pytorch/linux-64/
-@call conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/ 
-
-@call conda install -y -c conda-forge cudatoolkit=11.8
-
-@call pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+@call pip config set global.index-url https://mirrors.cloud.tencent.com/pypi/simple
 @call pip install torch==2.1.2 torchvision==0.16.2 --extra-index-url https://download.pytorch.org/whl/cu118
 @call pip3 install -U iopaint
 @call iopaint install-plugins-packages
