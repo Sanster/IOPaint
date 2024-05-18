13f8e229994f48998db0fd85b2105581326daecb
Qing <cwq1913@gmail.com>
2024-05-18 15:48:30 +0800

    Update win_config_cn.bat

--- scripts/user_scripts/win_config_cn.bat
+++ scripts/user_scripts/win_config_cn.bat
@@ -6,7 +6,7 @@ set PATH=C:\Windows\System32;%PATH%
 
 @call conda-unpack
 
-@call pip config set global.index-url https://mirrors.cloud.tencent.com/pypi/simple
+@call pip config set global.extra-index-url "https://pypi.tuna.tsinghua.edu.cn/simple https://mirrors.cloud.tencent.com/pypi/simple"
 @call pip install torch==2.1.2 torchvision==0.16.2 --extra-index-url https://download.pytorch.org/whl/cu118
 @call pip3 install -U iopaint
 @call iopaint install-plugins-packages
